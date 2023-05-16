1b3e6cbc848d738b42bb7870f6a9c2a7e59ece70
ParkJaeYoung <izocuna@gmail.com>
2023-05-16 15:47:29 +0900

    Implement bubble sort
    버블소트 메인 구현완료

---
+++ src/test/java/org/fastcampus/MainTest.java
@@ -0,0 +1,12 @@
+package org.fastcampus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+  @Test
+  void test1() {
+    Main.main(null);
+  }
+}
